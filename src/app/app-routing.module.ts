abfab4e7c882f8acdd726d02a1a71a437608b8e6
Yann Arvid <arvid.schoelhorn@gmail.com>
2023-07-09 19:00:09 +0200

    add component tree-visualization

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { TreeVisualizationComponent } from './tree-visualization/tree-visualization.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/tree-visualization', pathMatch: 'full'},
+  { path: 'tree-visualization', component: TreeVisualizationComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/tree-visualization/tree-visualization.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-tree-visualization',
+  templateUrl: './tree-visualization.component.html',
+  styleUrls: ['./tree-visualization.component.scss']
+})
+export class TreeVisualizationComponent {
+
+}
