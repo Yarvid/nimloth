c3020673c6c5b3b48b85abac0ea720008fffc553
Yann Arvid <arvid.schoelhorn@gmail.com>
2023-07-10 21:57:21 +0200

    add person model and serializer

---
+++ backend/persons/serializers.py
@@ -0,0 +1,9 @@
+from rest_framework import serializers
+from persons.models import Person
+
+class PersonSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Person
+        fields = ['id', 'first_name', 'last_name', 'date_of_birth']
+        
